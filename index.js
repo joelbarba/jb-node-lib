144ce1055b8028f4d8072ae03d5cbf1365e3147e
joel.barba <joel.barba.vidal@gmail.com>
2024-10-04 09:24:12 +0200

    Added color specific functions

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const readline = require('readline');
 
+function sleep(ms) { return new Promise(resolve => setTimeout(resolve, ms)) }
+
 function move(x = 1, y = 1) { readline.cursorTo(process.stdout, x, y); }
 
 function print(text = '', options) {
@@ -10,6 +12,9 @@ function print(text = '', options) {
   if (ops.color) { text = color(text, ops.color, ops.effect, ops.bgColor); }
   process.stdout.write(text);
 }
+
+// Returns a string with 'char' repeated 'num' times
+function repeat(num, char) { return Array.from(Array(num)).map(_ => char).join(''); }
 
 const reset = `\x1b[0m`;
 const allEffects = {
@@ -36,10 +41,33 @@ function color(text, color = 'green', effect = 'bright', bgColor = '') {
   const effectCodes = effect.split(' ').map(eff => allEffects[eff] || '').join('');
   return `${effectCodes}${allColors[color] || ''}${allColors['bg_' + bgColor] || ''}${text}${reset}`;
 }
-function sleep(ms) { return new Promise(resolve => setTimeout(resolve, ms)) }
+
+function setColor(color = 'green', effect = 'bright', bgColor = '') {
+  const effectCodes = effect.split(' ').map(eff => allEffects[eff] || '').join('');
+  let code = '';
+  if (effect)  { code += (effectCodes || ''); }
+  if (color)   { code += (allColors[color] || ''); }
+  if (bgColor) { code += (allColors['bg_' + bgColor] || ''); }
+  process.stdout.write(code);
+}
+function resetColor() {
+  process.stdout.write(reset);
+}
 
 
-module.exports.move   = move;
-module.exports.print  = print;
-module.exports.color  = color;
-module.exports.sleep  = sleep;
+module.exports.move       = move;
+module.exports.print      = print;
+module.exports.repeat     = repeat;
+module.exports.color      = color;
+module.exports.setColor   = setColor;
+module.exports.resetColor = resetColor;
+module.exports.red        = function(text) { return color(text, 'red'    ); };
+module.exports.green      = function(text) { return color(text, 'green'  ); };
+module.exports.yellow     = function(text) { return color(text, 'yellow' ); };
+module.exports.blue       = function(text) { return color(text, 'blue'   ); };
+module.exports.gray       = function(text) { return color(text, 'white', 'dim'); };
+module.exports.grayDark   = function(text) { return color(text, 'gray'   ); };
+module.exports.cyan       = function(text) { return color(text, 'cyan'   ); };
+module.exports.black      = function(text) { return color(text, 'black',  ''); };
+module.exports.brown      = function(text) { return color(text, 'yellow', ''); };
+module.exports.white      = function(text) { return color(text, 'white'  ); };
