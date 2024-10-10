c39100f8ba63911711141b7a0119b2a34420b87a
joel.barba <joel.barba.vidal@gmail.com>
2024-10-10 12:51:31 +0200

    template updated

--- template.js
+++ template.js
@@ -25,8 +25,8 @@ function exit() {
 }
 
 const maxWidth = 100;
-let pos = 1;
-const opts = [
+let pos = 0;
+const menu = [
   { code: 'op1', title: `Option 1` },
   { code: 'op2', title: `Option 2` },
   { code: 'op3', title: `Option 3` },
@@ -42,123 +42,65 @@ console.clear();
 
 printMenu();
 keyboard.push({
-  keyUp   : () => { if (pos > 1) { pos--; printMenu(); } },
-  keyDown : () => { if (pos < opts.length) { pos++; printMenu(); } },
-  keyEnter: () => { selectMainMenuOption(opts[pos-1]); },
+  keyUp   : () => { if (pos > 0) { pos--; printMenu(); } },
+  keyDown : () => { if (pos < menu.length - 1) { pos++; printMenu(); } },
+  keyEnter: () => { selectMainMenuOption(menu[pos]); },
 });
 
 function printMenu() {  
   print(`Select Option:`, 0, 0);
-  print(`Raspberry Pi IP = ` + green(ip), 25, 0);
-  for (let t = 0; t < opts.length; t++) {
-    const opt = opts[t];
-    let txt = line((t+1) + '.', 5, opt.title, 9, opt.com(), 25, opt.desc, 75);
-    if (pos === t + 1) { txt = color(txt, 'white', 'bright', 'yellow'); }
-    print(txt, 0, t + 2);
+  for (let t = 0; t < menu.length; t++) {
+    const option = menu[t];
+    let txt = line((t+1) + '.', 5, option.title, 9);
+    if (pos === t) { txt = color(txt, 'white', 'bright', 'yellow'); }
+    print(txt, 1, t + 2);
   }
-
-  print(yellow(`--> `), 0, pos + 1);
+  print(yellow(`--> `), 1, pos + 2);
 }
 
 
-
-
-
-
-const top = opts.length + 5;
+let top = menu.length + 5;
 async function selectMainMenuOption(opt) {
-  for (let t = 0; t < 20; t++) { print(repeat(maxWidth, ' '), 0, opts.length + 5 + t); } // Clear screen
-  print(yellow(opt.com()), 0, top);
+  for (let t = 0; t < 20; t++) { print(repeat(maxWidth, ' '), 0, menu.length + 5 + t); } // Clear screen
+  print(opt.code + ' selected', 0, top);
   move(0, top + 1);
   keyboard.push({}); // Disable menu
-  if (opt.code === 'start')       { await deatachXTerm('start'); }
-  if (opt.code === 'stop')        { await terminateSH(); }
-  if (opt.code === 'pingPi')      { await pingPi();  }
-  if (opt.code === 'pingApp')     { await pingApp(); }
-  if (opt.code === 'scan')        { await scanIPs(); }
-  if (opt.code === 'check')       { await checkMain(); }
-  if (opt.code === 'activate')    { await activation('activate'); }
-  if (opt.code === 'deactivate')  { await activation('deactivate'); }
-  if (opt.code === 'update')      { await updateSH(``); }
-  if (opt.code === 'killAll')     { await killAllXTerm(); }
-  if (opt.code === 'shutdown')    { await shutDown(); }
-  if (opt.code === 'tail')        { await deatachXTerm('tail'); }
-  if (opt.code === 'ssh')         { await deatachXTerm('ssh'); }
+  if (opt.code === 'op1')   { /* ... */ }
+  if (opt.code === 'op2')   { /* ... */ }
+  if (opt.code === 'op3')   { /* ... */ }
+  if (opt.code === 'op4')   { /* ... */ }
+  if (opt.code === 'op5')   { /* ... */ }
+  if (opt.code === 'op6')   { /* ... */ }
+  if (opt.code === 'op7')   { /* ... */ }
+  if (opt.code === 'op8')   { await subMenu(); }
   keyboard.pop(); // Back to main menu keyboard
   printMenu();
 }
 
-
-
-
-async function pingPi() {
-  const res = await cmd(`ping -c 1 ${ip} | head -2`);
-  if (res.indexOf(`Destination Host Unreachable`) >= 0) {
-    print(red(res), 0, top + 2);
-  } else {
-    print(green(res), 0, top + 2);
-  }
-  // PING 192.168.1.132 (192.168.1.132) 56(84) bytes of data.                                            
-  // OK = 64 bytes from 192.168.1.132: icmp_seq=1 ttl=64 time=62.4 ms 
-  // KO = From 192.168.1.137 icmp_seq=1 Destination Host Unreachable       
-}
-
-async function pingApp() {
-  return cmd(`curl -s -X GET http://${ip}:4358/ping`).then(res => {
-    print(green(res + ': The main.js process is running on the Raspberry Pi'), 0, top + 2);
-  }).catch(err => {
-    print(red(err), 0, top + 2);
-  });
-}
-
-async function activation(verb) {
-  return cmd(`curl -s -X GET http://${ip}:4358/${verb}`).then(res => {
-    print('The Alarm is now: ' + green(res), 0, top + 2);
-  }).catch(err => {
-    print(red(err), 0, top + 2);
-  });
-}
-
-async function scanIPs() {
-  // print(yellow(`nmap -sn 192.168.1.0/24 ...`), 0, top);
-  const res = await cmd(`nmap -sn 192.168.1.0/24 | grep "scan report"`);
-  let ips = []; // Nmap scan report for 192.168.1.128
-  res.split(`\n`).filter(r => !!r).forEach(r => {
-    const ip = r.split('Nmap scan report for ')[1];
-    if (ip && ip.split('.').every(n => n.length <= 3)) { ips.push(ip); }
-  });
-
-  let sel = 0;
-  print(`The following IPs are detected on the local network. Select one (${cyan('Enter')})`, 0, top + 1);
-  print(`or press ${cyan('t')} to test a curl -X GET http://$ip:4358/ping on the IP`, 0, ips.length + 4);
-  printIps();
-  function printIps() {
-    for (let t = 0; t < ips.length; t++) {
-      let txt = ips[t];
-      if (t === sel) { txt = color(ips[t], 'white', 'bright', 'yellow'); }
+async function subMenu() {
+  let pos2 = 0;
+  print(`This is a submenu. Select one (${cyan('Enter')})`, 0, top + 1);
+  const subMenu = [
+    { code: 'sop1', title: `Sub Option 1` },
+    { code: 'sop2', title: `Sub Option 2` },
+    { code: 'sop3', title: `Sub Option 3` },
+    { code: 'sop4', title: `Sub Option 4` },
+  ];
+  function printSubMenu() {
+    for (let t = 0; t < subMenu.length; t++) {
+      let txt = subMenu[t].title;
+      if (t === pos2) { txt = color(txt, 'white', 'bright', 'yellow'); }
       print('     ' + txt, 2, top + 4 + t);
     }
-    print('-->', 2, top + 4 + sel);
+    print('-->', 2, top + 4 + pos2);
   }
+  printSubMenu();
   keyboard.push({
-    keyUp   : () => { if (sel > 0)          { sel--; printIps(); } },
-    keyDown : () => { if (sel < ips.length) { sel++; printIps(); } },
+    keyUp   : () => { if (pos2 > 0)                  { pos2--; printSubMenu(); } },
+    keyDown : () => { if (pos2 < subMenu.length - 1) { pos2++; printSubMenu(); } },
     keyEnter: () => {
-      ip = ips[sel];
-      fs.writeFileSync(LAST_IP_FILE, ip);
-      print(` <-- Selected`, 25, top + 4 + sel);
+      print(` <-- Selected`, 25, top + 4 + pos2);
       release();
-    },
-    ['t']: async () => {
-      print(`curl -X GET http://${ips[sel]}:4358/ping`, 25, top + 4 + sel);
-      cmd(`curl -s -X GET http://${ips[sel]}:4358/ping`).then(res => {
-        print(green(res) + repeat(80, ' '), 25, top + 4 + sel);
-        printIps();
-      }).catch(err => {
-        print(red(err), 25, top + 4 + sel);
-        if (sel < ips.length) { sel++ }
-        printIps();
-      });
     },
   });
   let release = () => {};
