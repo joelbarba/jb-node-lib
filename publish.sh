648cbc58b5098cd95eb4d0d3946099bc6d5a9d70
joel.barba <joel.barba.vidal@gmail.com>
2025-08-29 10:52:15 +0200

    New version published: 1.13.0

--- publish.sh
+++ publish.sh
@@ -3,22 +3,18 @@ oldVersion=$(node -p "require('./package.json').version");
 npm version minor --force
 newVersion=$(node -p "require('./package.json').version");
 
-git status
-
 if changes=$(git status --porcelain) && [[ -z "$changes" ]]; then
   echo "Working directory clean. Nothing to commit."
 else
   echo "There are changes to commit."
   git add -A
+  git status
   git commit -m "New version published: $newVersion"
   git push origin master
 fi
-
-
 
 echo ""
 echo "Update version: $oldVersion  --->  $newVersion"
 echo ""
 
-
-# npm publish
+npm publish
